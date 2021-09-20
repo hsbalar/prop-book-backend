9cb719ad719467ad29ed702a4caccc7b1d35625b
Hitesh Balar <hsbalar@gmail.com>
2021-09-20 08:38:26 +0530

    CORS optiones

--- server/index.js
+++ server/index.js
@@ -8,11 +8,21 @@ import routes from './routes';
 
 const app = express();
 
-app.use(
-  cors({
-    origin: 'https://property-book.netlify.app',
-  })
-);
+const whitelist = [
+  'https://property-book.netlify.app',
+  'http://localhost:3000',
+];
+const corsOptionsDelegate = function (req, callback) {
+  let corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
+
+app.use(cors(corsOptionsDelegate));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
